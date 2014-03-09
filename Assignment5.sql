ad5c5200ac7e6c5d0b4f1c7245f97697a95cb00f
aronquillo17 <Alex.Ronquillo17@gmail.com>
2014-03-09 09:06:19 -0400

    Initial Comments Update
    Added comments to describe the purpose of the doc. Also commented out drop table @ the end of the file so the queries work.

--- Assignment5.sql
+++ Assignment5.sql
@@ -1,3 +1,11 @@
+-- This file is to use for reference when dealing with how we will normalize our tables.
+-- The tables are still in development so we still need to discuss how to break them apart
+-- ToDo tags are placed around the file to indicate where we have not finished and need to further examine
+-- Once you think a ToDo section has been completed, type your initials in the comment next to it
+-- Once all our initials are recorded, we can delete the ToDo and repeat until the file, then the project is done
+
+-- ToDo: Set up shipper table which records data about a shipper
+-- Needs primary key. Determine if there are any other values which should be included in this table.
 CREATE TABLE SHIPPER
 (
 	ShipperPhone INT NOT NULL
@@ -6,6 +14,8 @@ CREATE TABLE SHIPPER
 	, ShipperContents VARCHAR(MAX) NOT NULL
 )
 
+-- ToDo: Set up shipment table which records data the shipment from the shipper to the store.
+-- Needs primary key. Determine if there are any other values which should be included in this table.
 CREATE TABLE SHIPMENT
 (
 	ShipmentNumber BIGINT NOT NULL
@@ -14,7 +24,8 @@ CREATE TABLE SHIPMENT
 	, Arrival DATE NULL
 )
 	
-
+-- ToDo: Set up items table which records data the item sold at the store
+-- Needs primary key. Determine if there are any other values which should be included in this table.
 CREATE TABLE ITEMS
 (
 	ShipmentNumber BIGINT NOT NULL
@@ -24,6 +35,8 @@ CREATE TABLE ITEMS
 	, StoreSalesperson CHAR(20) NOT NULL
 )
 
+-- ToDo: Set up store table which records data about a store - not the shipper -
+-- Needs primary key. Determine if there are any other values which should be included in this table.
 CREATE TABLE STORE
 (
 	StoreName VARCHAR(MAX) NOT NULL
@@ -31,5 +44,6 @@ CREATE TABLE STORE
 	, StoreSalesperson CHAR(20) NOT NULL
 	, ItemOrderDate DATE NOT NULL 
 )
-	
-	DROP TABLE SHIPMENT, STORE, ITEMS, SHIPPER
+
+-- Drops tables when changed	
+--DROP TABLE SHIPMENT, STORE, ITEMS, SHIPPER
